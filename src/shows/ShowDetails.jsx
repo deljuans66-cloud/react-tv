43d0d16311933427a2ea89362be26099785540f7
DelJuan Smith <deljuans66@gmail.com>
2025-11-09 18:08:19 -0500

    Actual completed Version on react-tv

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,21 +1,26 @@
-import "./episodes.css";
+import { useState } from "react";
 
-export default function EpisodeDetails({ episode }) {
-  if (!episode) {
-    return (
-      <section className="episode-details">
-        <h2>Episode Details</h2>
-        <p>Select an episode to learn more.</p>
-      </section>
-    );
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+
+import "./shows.css";
+
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  if (!show) {
+    return <p>Please select a show to learn more.</p>;
   }
 
   return (
-    <section className="episode-details">
-      <h2>Episode {episode.number}</h2>
-      <h3>{episode.title}</h3>
-      <p>{episode.description}</p>
-      <button>Watch now</button>
-    </section>
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
   );
 }
